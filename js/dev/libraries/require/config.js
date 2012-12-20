27927553bd52009f1367793060c3f76073a92ad3
Adam Draper <adamwdraper@gmail.com>
2012-12-19 22:19:03 -0700

    test

--- js/dev/libraries/require/config.js
+++ js/dev/libraries/require/config.js
@@ -2,7 +2,7 @@
  * Dev Config Settings
  */
 requirejs.config({
-    baseUrl: '/js/dev',
+    baseUrl: 'js/dev',
     paths: {
         'jqueryLib': [
             '//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min',
